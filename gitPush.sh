3b11253a9974bde41243cb8c07982aba8b33d1a3
sauloal <sauloal@yahoo.com.br>
2012-03-13 16:37:14 +0100

    2012_03_13_16_37_14_01_00

--- gitPush.sh
+++ gitPush.sh
@@ -7,15 +7,7 @@ gitnick=github_$gitrepo
 git config --global user.name "$gituser" 2>/dev/null
 git config --global user.email $gitemail 2>/dev/null
 git remote add $gitnick git@github.com:$gituser/$gitrepo.git
-git push $gitnick master:$branch
 
-#echo '*.gz'     >  .gitignore
-#echo '*komodo*' >> .gitignore
-
-#git init
-#git config user.name saulo
-#git config user.email sauloal@gmail.com
-#git remote add github git@github.com:sauloal/pythonscripts.git
 NAME=`date --rfc-3339=seconds | tr " |,|:|\-|\+" _`
 
 git add .
