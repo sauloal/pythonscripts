156c872d7c2680fbb26abbe000a3e6f9034644b4
sauloal <sauloal@yahoo.com.br>
2012-03-13 20:00:02 +0100

    2012-03-13_20_00_01_01_00 :: 0 :: M => 1

--- wrappers/jelly.py
+++ wrappers/jelly.py
@@ -34,6 +34,8 @@ from tools import *
 """
 
 
+
+
 class sampleWrapper():
     def __init__(self, name):
         self.name     = name
@@ -79,4 +81,133 @@ def sample(messaging):
 
 #sample = sampleWrapper("watever0")
 
-    
+"""
+count, stats, histo, dump, merge, query, cite, qhisto, qdump, qmerge, jf
+
+
+
+COUNT
+Usage: jellyfish count [options] file:path+
+
+Count k-mers or qmers in fasta or fastq files
+
+Options (default value in (), *required):
+ -m, --mer-len=uint32                    *Length of mer
+ -s, --size=uint64                       *Hash size
+ -t, --threads=uint32                     Number of threads (1)
+ -o, --output=string                      Output prefix (mer_counts)
+ -c, --counter-len=Length in bits         Length of counting field (7)
+     --out-counter-len=Length in bytes    Length of counter field in output (4)
+ -C, --both-strands                       Count both strand, canonical representation (false)
+ -p, --reprobes=uint32                    Maximum number of reprobes (62)
+ -r, --raw                                Write raw database (false)
+ -q, --quake                              Quake compatibility mode (false)
+     --quality-start=uint32               Starting ASCII for quality values (64)
+     --min-quality=uint32                 Minimum quality. A base with lesser quality becomes an N (0)
+ -L, --lower-count=uint64                 Don't output k-mer with count < lower-count
+ -U, --upper-count=uint64                 Don't output k-mer with count > upper-count
+     --matrix=Matrix file                 Hash function binary matrix
+     --timing=Timing file                 Print timing information
+     --stats=Stats file                   Print stats
+     --usage                              Usage
+ -h, --help                               This message
+     --full-help                          Detailed help
+ -V, --version                            Version
+
+
+
+
+
+STATS
+Usage: jellyfish stats [options] db:path
+
+Statistics
+
+Display some statistics about the k-mers in the hash:
+
+Unique:    Number of k-mers which occur only once.
+Distinct:  Number of k-mers, not counting multiplicity.
+Total:     Number of k-mers, including multiplicity.
+Max_count: Maximum number of occurrence of a k-mer.
+
+Options (default value in (), *required):
+ -L, --lower-count=uint64                 Don't consider k-mer with count < lower-count
+ -U, --upper-count=uint64                 Don't consider k-mer with count > upper-count
+ -v, --verbose                            Verbose (false)
+ -o, --output=c_string                    Output file
+     --usage                              Usage
+ -h, --help                               This message
+     --full-help                          Detailed help
+ -V, --version                            Version
+
+
+
+
+HISTO
+Usage: jellyfish histo [options] db:path
+
+Create an histogram of k-mer occurrences
+
+Create an histogram with the number of k-mers having a given
+count. In bucket 'i' are tallied the k-mers which have a count 'c'
+satisfying 'low+i*inc <= c < low+(i+1)*inc'. Buckets in the output are
+labeled by the low end point (low+i*inc).
+
+The last bucket in the output behaves as a catchall: it tallies all
+k-mers with a count greater or equal to the low end point of this
+bucket.
+
+Options (default value in (), *required):
+ -l, --low=uint64                         Low count value of histogram (1)
+ -h, --high=uint64                        High count value of histogram (10000)
+ -i, --increment=uint64                   Increment value for buckets (1)
+ -t, --threads=uint32                     Number of threads (1)
+ -f, --full                               Full histo. Don't skip count 0. (false)
+ -o, --output=c_string                    Output file
+ -v, --verbose                            Output information (false)
+     --usage                              Usage
+     --help                               This message
+     --full-help                          Detailed help
+ -V, --version                            Version
+
+
+
+
+DUMP
+Usage: jellyfish stats [options] db:path
+
+Dump k-mer counts
+
+By default, dump in a fasta format where the header is the count and
+the sequence is the sequence of the k-mer. The column format is a 2
+column output: k-mer count.
+
+Options (default value in (), *required):
+ -c, --column                             Column format (false)
+ -t, --tab                                Tab separator (false)
+ -L, --lower-count=uint64                 Don't output k-mer with count < lower-count
+ -U, --upper-count=uint64                 Don't output k-mer with count > upper-count
+ -o, --output=c_string                    Output file
+     --usage                              Usage
+ -h, --help                               This message
+ -V, --version                            Version
+
+
+
+MERGE
+Usage: jellyfish merge [options] input:c_string+
+
+Merge jellyfish databases
+
+Options (default value in (), *required):
+ -s, --buffer-size=Buffer length          Length in bytes of input buffer (10000000)
+ -o, --output=string                      Output file (mer_counts_merged.jf)
+     --out-counter-len=uint32             Length (in bytes) of counting field in output (4)
+     --out-buffer-size=uint64             Size of output buffer per thread (10000000)
+ -v, --verbose                            Be verbose (false)
+     --usage                              Usage
+ -h, --help                               This message
+ -V, --version                            Version
+
+
+"""
