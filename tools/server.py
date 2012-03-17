a7501a85b0b010fc5489716398c5a5a640dac649
sauloal <sauloal@yahoo.com.br>
2012-03-17 19:30:02 +0100

    2012-03-17_19_30_01_01_00 :: 0 :: M => 1

--- tools/server.py
+++ tools/server.py
@@ -1,10 +1,10 @@
 import SocketServer
 import threading
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-
+import signal
+import os
 
 import constants
-import os
 import glob
 
 
@@ -24,9 +24,8 @@ class jobServer(SocketServer.BaseRequestHandler):
         print "{} wrote:".format(self.client_address[0])
         print self.data
         # just send back the same data, but upper-cased
-        self.request.sendall(self.getHeader())
-        self.request.sendall(self.getTail())
-        self.request.sendall(self.data.upper())
+        self.wfile.write(self.getHeader())
+        self.wfile.write(self.getTail()  )
 
     def getAllPaths(self):
         dirs = []
@@ -56,33 +55,51 @@ class jobServer(SocketServer.BaseRequestHandler):
         return tail
 
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write("Hello World!")
-
 class serverDaemon(threading.Thread):
     def __init__(self):
         super(serverDaemon, self).__init__()
         self.HOST, self.PORT = "localhost", 9999
 
         # Create the server, binding to localhost on port 9999
-        self.serverInst = SocketServer.TCPServer((self.HOST, self.PORT), jobServer)
+        serverInst      = SocketServer.TCPServer((self.HOST, self.PORT), jobServer)
+        self.serverInst = serverInst
 
     def run(self):
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
+        print "running server"
         self.serverInst.serve_forever()
         print "i can do stuff me"
 
+    def stop(self):
+        #self.serverInst.close_request()
+        #self.serverInst.finish_request()
+        self.serverInst.server_close()
+        self.serverInst.shutdown()
 
-#Thread(target=serve_on_port, args=[1111]).start()
-#serve_on_port(2222)
 
+print "starting class"
+daemon = serverDaemon()
+
+
+def signal_handler(signal, frame):
+    print '!'*50
+    print "You've sent signal " + str(signal) + ". exiting"
+    print '!'*50
+    daemon.stop()
+    daemon.join()
+    print "finished"
+    sys.exit(signal)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == "__main__":
-    daemon = serverDaemon()
+    print "starting daemon"
     daemon.start()
+    print "daemon started"
+    print "now i can run more stuff"
+    print "including finishing it"
+    #daemon.stop()
+    #daemon.join()
+    #print "finished"
 
